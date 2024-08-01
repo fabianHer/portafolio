c9f892aa128d2fd6afc240003457d20af12da22f
Yilber Fabian Hernadez Rincon <yilber.hernandez@datatools.com.co>
2024-08-01 13:36:00 -0500

    Ajustes de variables que no se usan

--- src/components/pages/Educacion.jsx
+++ src/components/pages/Educacion.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { AppContext } from "../../context/AppContext";
 import { BsFillBuildingsFill } from "react-icons/bs";
 import { FaMedal, FaUniregistry } from "react-icons/fa";
