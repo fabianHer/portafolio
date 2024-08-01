d67ad92f8ce390da0dc96686153871e672f6e749
Yilber Fabian Hernadez Rincon <yilber.hernandez@datatools.com.co>
2024-08-01 13:53:48 -0500

    Ajustes de variables que no se usan

--- src/components/pages/Contactar.jsx
+++ src/components/pages/Contactar.jsx
@@ -29,14 +29,10 @@ export const Contactar = () => {
         </div>
         <div className="grid grid-cols-3 gap-4 mb-4">
           <div className="grid justify-items-end h-45">
-              <a rel="noreferrer">
                 <img src={linkedin} className="w-48 hover:w-52" alt="Linkedin" />
-              </a>
           </div>
           <div className="grid justify-center h-45">
-              <a rel="noreferrer">
                 <img src={githubIcon} className="w-48 hover:w-52" alt="GitHub" />
-              </a>
           </div>
           <div className="grid justify-star h-45">
           <a href="https://api.whatsapp.com/send/?phone=%573116204093&text=Hello+Fabian" target="_blank" rel="noreferrer">
--- src/components/pages/ProyectosPersonales.jsx
+++ src/components/pages/ProyectosPersonales.jsx
@@ -12,17 +12,17 @@ export const ProyectosPersonales = () => {
         <div className="p-4 sm:ml-64 bg-gray-500" onClick={closeMenu}>
           <div className="p-4 border-2 border-dashed rounded-lg border-gray-700">
           <div className="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
-            <div className="rounded-tr-lg bg-gray-800 h-auto cursor-pointer text-white hover:bg-dark-gray hover:border-green-500 border-2 sm:text-1xl md:text-3xl">    
+            <div className="rounded-tr-lg bg-gray-800 h-auto cursor-pointer text-white hover:bg-dark-gray hover:border-green-500 border-2 sm:text-1xl md:text-3xl">
                 <div className="grid grid-cols-2 m-5">
-                    <FaRegFolderOpen className="text-green-500" />   
+                    <FaRegFolderOpen className="text-green-500" />
                     <div className="grid justify-items-end">
-                        <a  href="https://gif-expert-fabian.netlify.app" target="_blank">
-                           <IoOpenOutline className="text-white" /> 
+                        <a  href="https://gif-expert-fabian.netlify.app" target="_blank" rel="noreferrer">
+                           <IoOpenOutline className="text-white" />
                         </a>
                     </div>
-                </div>             
-                    <p className="text-2xl font-bold m-5">gif-expert-fabian</p>   
-                    <p className="text-lg m-5">Aplicación react web para buscar gifs</p>   
+                </div>
+                    <p className="text-2xl font-bold m-5">gif-expert-fabian</p>
+                    <p className="text-lg m-5">Aplicación react web para buscar gifs</p>
                 <div className="flex flex-row m-5 gap-2">
                     <FaReact className="text-blue-500"/>
                     <FaHtml5 className="text-orange-500" />
@@ -32,15 +32,15 @@ export const ProyectosPersonales = () => {
             </div>
             <div className="rounded-tr-lg bg-gray-800 h-auto cursor-pointer text-white hover:bg-dark-gray hover:border-green-500 border-2 sm:text-1xl md:text-3xl">    
                 <div className="grid grid-cols-2 m-5">
-                    <FaRegFolderOpen className="text-green-500" />   
+                    <FaRegFolderOpen className="text-green-500" />
                     <div className="grid justify-items-end">
-                        <a  href="https://gif-expert-fabian.netlify.app" target="_blank">
-                           <IoOpenOutline className="text-white" /> 
+                        <a  href="https://gif-expert-fabian.netlify.app" target="_blank" rel="noreferrer">
+                           <IoOpenOutline className="text-white" />
                         </a>
                     </div>
-                </div>             
-                    <p className="text-2xl font-bold m-5">Cajero</p>   
-                    <p className="text-lg m-5">Aplicación que simula un cajero automático</p>   
+                </div>
+                    <p className="text-2xl font-bold m-5">Cajero</p>
+                    <p className="text-lg m-5">Aplicación que simula un cajero automático</p>
                 <div className="flex flex-row m-5 gap-2">
                     <IoLogoAngular className="text-red-500" />
                     <FaHtml5 className="text-orange-500" />
